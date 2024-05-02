b9268d9e9583cce7654d5d60c4c9ea9875cbffa7
muniraIbrahim <168729759+muniraIbrahim@users.noreply.github.com>
2024-05-02 15:12:58 +0200

    Add files via upload

---
+++ Seminar3/src/main/java/se/kth/iv1350/implementingpos/seminar3/integration/ExternalSystemCreator.java
@@ -0,0 +1,38 @@
+
+package se.kth.iv1350.linnea.seminar3.integration;
+
+/**
+ * This class creates and passes references to the external systems.
+ * 
+ */
+public class ExternalSystemCreator {
+    private ExternalAccountingSystem externalAccountingSystem;
+    private ExternalInventorySystem externalInventorySystem;
+    
+    /**
+    * Creates a new instance which creates the external layers.
+    */
+    public ExternalSystemCreator(){
+        this.externalAccountingSystem = new ExternalAccountingSystem();
+        this.externalInventorySystem = new ExternalInventorySystem();
+    }
+    
+    /**
+     * Returns the ExternalAccountingSystem.
+     * 
+     * @return the ExternalAccountingSystem.
+     */
+    public ExternalAccountingSystem getExternalAccountingSystem(){
+        return this.externalAccountingSystem;
+    }
+    
+    /**
+     * Returns the ExternalInventorySystem.
+     * 
+     * @return the ExternalInventorySystem.
+     */
+    public ExternalInventorySystem getExternalInventorySystem(){
+        return this.externalInventorySystem;
+    }
+    
+}
